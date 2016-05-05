4f1c8dfd886f58dd1fc9d61ce256e9e927ef5cdc
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:06:39 +0530

    Guessing Game
    A simple dice simulator based on RNG

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var outputField: UILabel!
+    
+    @IBAction func goButton(sender: AnyObject) {
+        let randNumber = Int(arc4random_uniform(5) + 1)
+        if textField.text!.isEmpty {
+            outputField.text = "Please enter a value"
+            return
+        }
+        let inputNumber = Int(textField.text!)!
+        if inputNumber >= 1 && inputNumber <= 6 {
+            if randNumber == inputNumber {
+                outputField.text = "Congratulations!! You have won the lottery!!"
+            }
+            else {
+                outputField.text = "Sorry it was \(randNumber)!! Please try again!!"
+            }
+        }
+        else {
+            outputField.text = "Please enter value between 1 and 6"
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
